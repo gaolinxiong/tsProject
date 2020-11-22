d7486390c7a70fd1021fcf3d592c012257b905ff
gaolinxiong <1906972240@qq.com>
2020-11-22 16:29:14 +0800

    feat: update

--- webpack.config.js
+++ webpack.config.js
@@ -47,15 +47,6 @@ const configFun = env => {
                     }
                 },
                 {
-                    test: /\.(js|vue)$/,
-                    loader: 'eslint-loader',
-                    enforce: 'pre',
-                    include: [path.resolve('src'), path.resolve('test')],
-                    options: {
-                        formatter: require('eslint-friendly-formatter')
-                    }
-                },
-                {
                     test: /\.js$/,
                     exclude: /node_modules/,
                     loader: 'babel-loader'
