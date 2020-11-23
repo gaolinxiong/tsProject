6e6254e9449f31ddfaa14e0da86c4c92e216ee3c
gaolinxiong <1906972240@qq.com>
2020-11-23 10:58:41 +0800

    feat: update

--- bin/lib.js
+++ bin/lib.js
@@ -2,10 +2,4 @@
 
 const { program } = require("commander")
 
-program.command('init [projectName]','init project')  // projectName 是一个可选参数
-    .description("初始化项目")
-    .option('-f --force','如果存在输入的项目目录，强制删除项目目录') // 添加一个选项
-    .action((projectName,cmd)=>{
-        console.log(projectName) // projectName 是我们输入的参数，
-        console.log(cmd.force)  // cmd是Command对象
-    })
+program.command('init [projectName]','init project')
