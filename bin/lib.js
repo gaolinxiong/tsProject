a346ee476f5c67367377b975d790f0c26e9efa07
gaolinxiong <1906972240@qq.com>
2020-11-23 11:05:24 +0800

    feat: update

--- bin/lib.js
+++ bin/lib.js
@@ -1,5 +1,23 @@
 #!/usr/bin/env node
 
+// const { program } = require("commander")
+//
+// program.command('init [projectName]','init project')
+const pkg = require("./../package.json")
 const { program } = require("commander")
 
-program.command('init [projectName]','init project')
+let projectName;
+let force;
+
+program.version(pkg.version,'-v --version')
+
+program.arguments('[projectName]') // 指定解析的参数
+    .description("初始化项目")
+    .option('-f --force','如果存在输入的项目目录，强制删除项目目录')
+    .action((name,cmd)=>{
+        projectName = name;
+        force = cmd.force;
+    });
+program.parse(process.argv);
+
+console.log(projectName,force)
