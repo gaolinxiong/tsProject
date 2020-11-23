5cede9cdb7cf04380635599ff600704d8d1cae43
gaolinxiong <1906972240@qq.com>
2020-11-23 10:57:31 +0800

    feat: update

--- bin/lib.js
+++ bin/lib.js
@@ -1,13 +1,8 @@
 #!/usr/bin/env node
 
 const { program } = require("commander")
-// const pkg = require("./../package.json")
-//
-// program.version(pkg.version,'-v --version')
-//
-// program.parse(process.argv)
 
-program.command('init [projectName]')  // projectName 是一个可选参数
+program.command('init [projectName]','init project')  // projectName 是一个可选参数
     .description("初始化项目")
     .option('-f --force','如果存在输入的项目目录，强制删除项目目录') // 添加一个选项
     .action((projectName,cmd)=>{
